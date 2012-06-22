34ca3102bcae6b36b35cc1df9c6821bb259dc1f4
Marcus Bannerman <m.bannerman@gmail.com>
2012-06-22 15:54:20 +0100

    Readded the tracking code

--- htdocs/index.php
+++ htdocs/index.php
@@ -176,6 +176,19 @@ if ($TOC)
     <!--[if lt IE 10]><link rel="stylesheet" href="/style/ie-css3-support.css" type="text/css"/><![endif]-->
     <link rel="icon" type="image/png" href="/images/favicon.png" />
     <title>DynamO Simulation Package</title>
+    <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-31464781-1']);
+      _gaq.push(['_trackPageview']);
+
+        (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		      })();
+
+    </script>
   </head>
   <body>
     <!-- SPACER TO COUNTER DODGY PAGE MARGIN INTERACTIONS -->
@@ -240,7 +253,6 @@ if ($TOC)
       });
     </script>
     <?php } ?>
-    <script type="text/javascript" async="" src="http://www.google-analytics.com/ga.js"></script>
     <script>
 /*!
 * screenfull.js
