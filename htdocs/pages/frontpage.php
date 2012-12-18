8b490a3623f130d394da8f8b1b223c5551efe896
Marcus Bannerman <m.bannerman@gmail.com>
2012-12-18 22:44:24 +0000

    More work on the site

--- htdocs/pages/frontpage.php
+++ htdocs/pages/frontpage.php
@@ -7,27 +7,38 @@
    }
    $pagetitle="Welcome";
    ?>
+<div style="float:right; padding: 15px;">
+  <img src="/images/frontpage.png" width="250" height="269" alt="A simulation of hopper flow." />
+</div>
 <p>
-  DynamO is a free and open-source event-driven particle simulator.
+  DynamO is a free and open-source event-driven particle
+  simulator. Event-driven simulation is a fast and <u>analytical</u>
+  technique for particle simulation and has many advantages over the
+  more traditional time-stepping approaches (such as those found in
+  <a href="http://www.gromacs.org/">Gromacs</a>, <a href="http://web678.public1.linz.at/Drupal/?q=OpenSourceDEM">Liggghts</a>,
+  and <a href="http://www.ks.uiuc.edu/Research/namd/">NAMD</a>.  As
+  DynamO is a research testbed for this approach, it implements the
+  latest in event-driven algorithms and techniques.
 </p>
 <p>
-  It can be used to simulate a wide range of particle models, from
-  hard spheres, to square-wells and stepped potentials, thin needles
-  and more. From molecular dynamics to granular dynamics, DynamO
-  implements the latest in event-driven algorithms and
-  techniques.
+  You can use DynamO as a molecular dynamics package to study model
+  fluids or as a granular dynamics package to investigate solid
+  particle processes (such as the hopper flow to the right). A wide
+  range of particle models are available, from the basic hard sphere,
+  to square-wells, stepped Lennard-Jones potentials, thin needles
+  and more.
 </p>
 <p>
   Take a look at the
-  <a href="/index.php/features">features</a> to see if DynamO can
-  simulate what you need, <a href="/index.php/download">download</a> a
-  copy, and take a look at
-  the <a href="/index.php/documentation">documentation</a> on how to
-  install and use it.
+  <a href="/index.php/features">features / gallery</a> section of the
+  website to see examples of what systems DynamO can simulate. You
+  can <a href="/index.php/download">download</a> a copy, and take a
+  look at the <a href="/index.php/documentation">documentation</a> to
+  evaluate it for yourself.
 </p>
 
 <!-- Page End -->
-<h1>Latest News</h1>
+<h1 style="clear:both;">Latest News</h1>
 <?php 
 //Look in the news directory and create a date sorted list of the news items
 $file_array=glob('pages/news/*.html');
