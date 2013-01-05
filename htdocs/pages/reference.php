26e3500937891c28f088522f30a82d7dd2309eaa
Marcus Bannerman <m.bannerman@gmail.com>
2013-01-05 16:42:07 +0000

    More work on the reference documentation

--- htdocs/pages/reference.php
+++ htdocs/pages/reference.php
@@ -70,8 +70,8 @@
   <b>Example Usage:</b>
 </p>
 <p>
-  The following example <b>IDRange</b> includes the particle IDs
-  0, 1, 2, 3, 4, and 5.
+  The following example <b>IDRange</b> includes the particle IDs 0, 1,
+  2, 3, 4, and 5.
 </p>
 <?php codeblockstart();?><IDRange Type="Ranged" Start="0" End="5"/><?php codeblockend("brush: xml;"); ?>
 <p>
@@ -118,7 +118,8 @@
       value <i>"List"</i> to select this range type.
     </li>
     <li>
-      <b>ID</b> <i>(tag)</i>: A tag containing an ID/entry in the list.
+      <b>ID</b> <i>(tag)</i>: A tag containing an ID/entry in the
+      list.
       <ul>
 	<li>
 	  <b>val</b> <i>(attribute)</i>: The ID of the entry.
@@ -128,6 +129,190 @@
   </ul>
 </p>
 <h1>IDPairRange</h1>
+<p>
+  <b>IDPairRange</b>s are used to specify the pairs of particles to
+  which an Interaction applies. Each IDPairRange represent a
+  collection or set of ID pairs.
+</p>
+<h2>Type="All"</h2>
+<p>
+  <b>Description:</b> An ID Range which maps to all pairs of particles
+  in the simulation.
+</p>
+<p>
+  <b>Example Usage:</b>
+</p>
+<?php codeblockstart();?><IDPairRange Type="All"/><?php codeblockend("brush: xml;"); ?>
+<p>
+  <b>Full Tag, Subtag, and Attribute List</b>:
+  <ul>
+    <li>
+      <b>Type</b> <i>(attribute)</i>: Must have the value <i>"All"</i>
+      to select this range type.
+    </li>
+  </ul>
+</p>
+<h2>Type="None"</h2>
+<p>
+  <b>Description:</b> An ID Range which doesn't map to any pair of
+  particles.
+</p>
+<p>
+  <b>Example Usage:</b>
+</p>
+<?php codeblockstart();?><IDPairRange Type="None"/><?php codeblockend("brush: xml;"); ?>
+<p>
+  <b>Full Tag, Subtag, and Attribute List</b>:
+  <ul>
+    <li>
+      <b>Type</b> <i>(attribute)</i>: Must have the
+      value <i>"None"</i> to select this range type.
+    </li>
+  </ul>
+</p>
+<h2>Type="Pair"</h2>
+<p>
+  <b>Description:</b> This creates a IDPairRange from two
+  IDRanges. Each particle ID in the first range is paired with all of
+  the particle IDs in the second range. Pairs of particle IDs which
+  only match one range are not included in the pair range.
+</p>
+<p>
+  <b>Example Usage:</b> This example pairs particle IDs 0,1,2 with IDs
+  3,4,5. Pairs only within one range, such as (0,1) or (3,4), are not
+  included. All IDRangePairs are symmetric, so both (1,4) and (4,1)
+  are included in this example range.
+</p>
+<?php codeblockstart();?>
+<IDPairRange Type="Pair">
+  <IDRange Type="Ranged" Start="0" End="2"/>
+  <IDRange Type="Ranged" Start="3" End="5"/>
+</IDPairRange>
+<?php codeblockend("brush: xml;"); ?>
+<p>
+  <b>Full Tag, Subtag, and Attribute List</b>:
+  <ul>
+    <li>
+      <b>Type</b> <i>(attribute)</i>: Must have the
+      value <i>"Pair"</i> to select this range type.
+    </li>
+    <li>
+      <b>IDRange</b> <i>($2\times$tag)</i>: There must be two IDRange tags
+      present and each specifies one of the IDRanges used to make the
+      IDPairRange. The order in which the IDRanges are specifed is
+      unimportant. See the <a href="#idrange">section on the available
+      IDRange types</a> for more information.
+    </li>
+  </ul>
+</p>
+<h2>Type="Single"</h2>
+<p>
+  <b>Description:</b> This creates a IDPairRange from one
+  IDRange. Every particle ID in the IDRange is paired with every other
+  particle in the IDRange.
+</p>
+<p>
+  <b>Example Usage:</b> This example pairs particle IDs 0, 1, and 2.
+</p>
+<?php codeblockstart();?>
+<IDPairRange Type="Single">
+  <IDRange Type="Ranged" Start="0" End="2"/>
+</IDPairRange>
+<?php codeblockend("brush: xml;"); ?>
+<p>
+  <b>Full Tag, Subtag, and Attribute List</b>:
+  <ul>
+    <li>
+      <b>Type</b> <i>(attribute)</i>: Must have the
+      value <i>"Single"</i> to select this range type.
+    </li>
+    <li>
+      <b>IDRange</b> <i>(tag)</i>: See
+      the <a href="#idrange">section on the available IDRange
+      types</a> for more information.
+    </li>
+  </ul>
+</p>
+<h2>Type="List"</h2>
+<p>
+  <b>Description:</b> This is the most versatile IDPairRange available
+  as it allows you to specify the particle pairings individually.
+</p>
+<p>
+  <b>Example Usage:</b> This example matches the particle pairings
+  (0,1), (1,2), and (2,3). As IDPairRanges are symmetric, the pairings
+  (1,0), (2,1), and (3,2) are also included. This example might be
+  used to bond particles together into a chain.
+</p>
+<?php codeblockstart();?>
+<IDPairRange Type="List">
+  <IDPair ID1="0" ID2="1"/>
+  <IDPair ID1="1" ID2="2"/>
+  <IDPair ID1="2" ID2="3"/>
+</IDPairRange>
+<?php codeblockend("brush: xml;"); ?>
+<p>
+  <b>Full Tag, Subtag, and Attribute List</b>:
+  <ul>
+    <li>
+      <b>Type</b> <i>(attribute)</i>: Must have the
+      value <i>"List"</i> to select this range type.
+    </li>
+    <li>
+      <b>IDPair</b> <i>(tag)</i>: Each IDPair tag specifies one
+      pairing of particle IDs.
+      <ul>
+	<li>
+	  <b>ID1</b> <i>(attr)</i>: The first ID of the pair. The
+	  order of the IDs is unimportant and DynamO always writes the
+	  configuration out so that ID1 is the lowest of the pair.
+	</li>
+	<li>
+	  <b>ID2</b> <i>(attr)</i>: The second ID of the pair.
+	</li>
+      </ul>
+    </li>
+  </ul>
+</p>
+<h2>Type="Chains"</h2>
+<p>
+  <b>Description:</b> This IDPairRange is provided as a convenient way
+  to bond sequential particle IDs into linear chains.
+</p>
+<p>
+  <b>Example Usage:</b> If the example below was used with a square
+  bond Interaction, it would bond a set of particles together into
+  chains of 4.
+  
+  This example matches the particle pairings (0,1), (1,2), (2,3) and
+  (4,5), (5,6), (6,7). As IDPairRanges are symmetric, the pairings
+  (1,0), (2,1), etc. are also included.
+</p>
+<?php codeblockstart();?>
+<IDPairRange Type="Chains" Start="0" End="7" Interval="4"/>
+<?php codeblockend("brush: xml;"); ?>
+<p>
+  <b>Full Tag, Subtag, and Attribute List</b>:
+  <ul>
+    <li>
+      <b>Type</b> <i>(attribute)</i>: Must have the
+      value <i>"Chains"</i> to select this range type.
+    </li>
+    <li>
+      <b>Start</b> <i>(attribute)</i>: The first ID of the sequential
+      particle IDs.
+    </li>
+    <li>
+      <b>End</b> <i>(attribute)</i>: The last ID of the sequential
+      particle IDs.
+    </li>
+    <li>
+      <b>Interval</b> <i>(attribute)</i>: How long the chains
+      are. Please note that End - Start must be a whole multiple of
+      Interval.
+    </li>
+  </ul>
+</p>
 <h1>Scheduler</h1>
 <h2>Type="Dumb"</h2>
 <h2>Type="NeighbourList"</h2>
