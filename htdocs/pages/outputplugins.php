28684510000e3e2a655d19bbfce39c0038f1f8d7
Marcus Bannerman <m.bannerman@gmail.com>
2013-07-02 13:02:10 +0100

    Updated the comment about the thermal conductivity

--- htdocs/pages/outputplugins.php
+++ htdocs/pages/outputplugins.php
@@ -839,10 +839,10 @@ dynarun config.xml -c 1000000 -L MFT:BinWidth=0.5,Length=100
 </ul>
 <p>
   <b>Restrictions</b>: This form is only valid in the micro-canonical
-  ensemble (NVE).  The current implementation is also only valid
-  for systems with no interaction energies (athermal systems) and
-  without rotational degrees of freedom (such as the hard sphere
-  fluid), as it uses the approximation $e_i\approx m\,v^2_i/2$
+  ensemble (NVE).  The current implementation is also only valid for
+  systems without rotational degrees of freedom (such as the hard
+  sphere, square-well, stepped-potential fluids), as it uses the
+  approximation $e_i\approx m\,v^2_i/2+U_{i,config}$
   (see <a href="https://github.com/toastedcrumpets/DynamO/issues/23">issue
   #23</a> on the bug tracker).
 </p>
