0ff01baa13f9a6eb6f97d99e91b01986dd9c7a16
Marcus Bannerman <m.bannerman@gmail.com>
2018-02-24 23:30:02 +0000

    Updated the download instructions for dynamo

--- htdocs/pages/download.php
+++ htdocs/pages/download.php
@@ -15,85 +15,14 @@
    <a href="#prebuilt">prebuilt packages</a> for a range of Linux distributions.
 </p>
 
-<h1 id="prebuilt">Prebuilt packages</h1>
+<h1 id="prebuilt">Prebuilt Windows/Linux/OSX packages</h1>
 <p>
-  The DynamO source code is built regularly for a range of popular
-  Linux distributions.
+  <a href="https://github.com/toastedcrumpets/DynamO/releases/">Click here to see all releases</a>.
 </p>
-<?php
-function endswith($string, $test) {
-  $strlen = strlen($string);
-  $testlen = strlen($test);
-  if ($testlen > $strlen) return false;
-  return substr_compare($string, $test, $strlen - $testlen, $testlen) === 0;
-  }
-
-function YesNotd($value) {
-   if ($value){
-      echo "<td class=\"yes\">Yes</td>";
-   } else {
-      echo "<td class=\"no\">No</td>";
-   }
-  }
-
-echo "<table id=\"download-table\">\n";
-echo "<thead><tr><th>Distribution</th><th>Version</th><th>Build date</th><th>Visualiser</th><th>Adv. Potentials</th></tr></thead>\n";
-try {
-  $packagetypes = array(
-    array("extension" => "ubuntu14.04.deb", "distro" => "Ubuntu 14.04", "vis" => True, "c++11" => True),
-    array("extension" => "ubuntu12.04.deb", "distro" => "Ubuntu 12.04", "vis" => True, "c++11" => False), 
-    array("extension" => "centos7.rpm", "distro" => "CentOS 7 (RedHat Enterprise Linux)", "vis" => False, "c++11" => False),
-    array("extension" => "centos6.6.rpm", "distro" => "CentOS 6.6 (RedHat Enterprise Linux)", "vis" => False, "c++11" => False),
-    array("extension" => "fedora21.rpm", "distro" => "Fedora 21", "vis" => True, "c++11" => True),
-    array("extension" => "debian7.deb", "distro" => "Debian 7 (Wheezy)", "vis" => True, "c++11" => False),
-    array("extension" => "opensuse13.2.rpm", "distro" => "OpenSUSE 13.2", "vis" => True, "c++11" => True));
-
-  $filecount=0;
-  echo "<tbody>\n";
-  foreach($packagetypes as $data) {
-    $buildfiles = glob('build-uploads/*'.$data["extension"]);
-    if(!empty($buildfiles)) {
-      array_multisort(array_map( 'filemtime', $buildfiles), SORT_NUMERIC,SORT_DESC, $buildfiles);
-      $file=$buildfiles[0];
-      preg_match("#dynamomd-([0-9\\.]+)#", $file, $matches);
-      echo "<tr onclick=\"window.document.location='/".$file."'\">";
-      echo "<td>".$data["distro"]."</td>";
-      echo "<td>".$matches[1]."</td>";
-      echo "<td>".date("d/m/y", filemtime($file))."</td>";
-      YesNotd($data["vis"]);
-      YesNotd($data["c++11"]);
-      echo "</tr>\n";
-      $filecount+=1;
-    }
-  }
-  echo "</tbody>\n";
-  if ($filecount==0) {
-   echo "<tr><th colspan=\"5\">No builds found!</td></tr>";
-  }
-}
-catch (RuntimeException $e) {
-  header('Content-Type: text/plain; charset=utf-8');
-  echo "<li>Failed to load the prebuilt packages. ";
-  echo $e->getMessage()."</li>\n";
-}
-echo "</table>\n";
-?>
 <p>
   Please note, you may need to install additional packages for DynamO
-  to work, and not all functionality is supported across all
-  platforms. Only more recent distributions support the visualiser
-  (due to requirements on OpenGL libraries) and advanced potentials
-  (due to the required compiler support of C++11).
+  to work.
 </p>
-<p>
-  You can check the status of the builds using
-  the <a href="http://cdash.dynamomd.org/index.php?project=DynamO">DynamO
-  CDash site</a>. If your distribution or version is not listed above
-  and you think it should be,
-  please <a href="index.php/support#ContactingtheDevelopers">contact
-  the developers</a> to request an additional build,
-  or <a href="/index.php/tutorial1">compile the code yourself</a>.
-  <h1 id="prebuilt-ubuntu">Ubuntu PPA</h1>
 <p>
   A Ubuntu PPA is available which enables automatic updates of DynamO.
   To install, add the PPA to your system and install the dynamo
@@ -106,7 +35,6 @@ echo "</table>\n";
 sudo apt-get update
 sudo apt-get install dynamomd
 <?php codeblockend("bash"); ?>
-
 <h1>Source Code</h1>
 <p>
   The code is hosted on
